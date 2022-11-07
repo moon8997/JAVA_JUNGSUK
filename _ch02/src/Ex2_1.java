dba7de9eed574c67acc5711108ef0b534d46f84b
moon8997 <tjdansrlf@gmail.com>
2022-11-07 16:36:06 +0900

    first commit

---
+++ _ch02/src/Ex2_1.java
@@ -0,0 +1,12 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		System.out.println(3+5);
+		System.out.println(3-5);
+		System.out.println(3*5);
+		System.out.println(3/5.0);
+		
+	}
+
+}
---
+++ _ch02/src/VarEx2.java
@@ -0,0 +1,22 @@
+
+public class VarEx2 {
+
+	public static void main(String[] args) {
+		final int score; // = 100;
+		score = 200;
+		boolean power = true;
+		System.out.println(power);
+		
+		
+		char ch = 'A';
+		int i = 'A';
+		String str = ""; // 빈문자열
+		String str2 = "asd"; 
+		String str3 = str + str2;
+		System.out.println(str3);
+		
+		System.out.println(""+7+7);
+		System.out.println(7+7+"");
+	}
+
+}
