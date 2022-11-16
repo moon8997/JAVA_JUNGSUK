3bbc874e0a881810c85e4b84e719fe71a0d2a0f5
moon8997 <tjdansrlf@gmail.com>
2022-11-16 18:01:11 +0900

    조건문

---
+++ _ch04/src/Ex4_1.java
@@ -0,0 +1,14 @@
+public class Ex4_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score = 80;
+		
+		if(score >= 60) {
+			System.out.println("합겹입니다.");
+		}
+	}
+
+}
+// if 문 
+// - 조건이 참(true) 일때, 괄호 {} 안의 문장들을 수행한다.
