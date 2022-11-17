12a478ddc115ee450e472422e5a2ec34bf5e1687
moon8997 <tjdansrlf@gmail.com>
2022-11-17 19:05:45 +0900

    if else 문

---
+++ _ch04/src/Ex4_3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex4_3 {
+
+	public static void main(String[] args) {
+		System.out.print("0~9 중 정수를 적어주세요. >> ");
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt(); 
+
+		if(input==0) {
+			System.out.println("0을 입력하였습니다.");	
+		} else { // input!=0�� ���
+			System.out.println("0이 아닌 다른 수를 입력하였습니다.");
+		}
+	}
+
+}
