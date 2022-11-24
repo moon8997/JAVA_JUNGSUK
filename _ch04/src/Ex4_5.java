027e5b1c6248122016012fae9a9c6393cfb086d5
moon8997 <tjdansrlf@gmail.com>
2022-11-24 18:05:01 +0900

    else if

---
+++ _ch04/src/Ex4_5.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Ex4_5 {
+
+	public static void main(String[] args) {
+		int score = 0; // 점수를 저장하기 위한 변수
+		char grade = ' ', opt = '0'; 
+		
+		System.out.print("점수를 입력하세요. >> ");
+		
+		Scanner scanner = new Scanner(System.in);
+		score = scanner.nextInt(); // 화면을 통해 입력받은 점수를 score에 저장
+		
+		System.out.printf("당신의 점수는 %d입니다. %n", score);
+		
+		if(score >= 90) {
+			grade = 'A';
+			if (score >= 98) {
+				opt = '+';
+			} else if(score < 94) {
+				opt = '-';
+			}
+		} else if(score >= 80) {
+			grade = 'B';
+			if (score >= 88) {
+				opt = '+';
+			} else if(score < 84) {
+				opt = '-';
+			}
+		} else if(score >= 70) {
+			grade = 'C';
+			if (score >= 78) {
+				opt = '+';
+			} else if(score < 74) {
+				opt = '-';
+			}
+		} 
+		else {
+			grade = 'D';
+		}
+		
+		System.out.printf("당신의 학점은 %c%c입니다.", grade, opt);
+	}
+
+}
